637285062d85fa9fee3073e5ce39d17667281fd4
amarouane-ABDELHAK <am0089@uah.edu>
2026-01-29 14:09:58 -0600

    Add black-friday-load

---
+++ env.example.sh
@@ -0,0 +1,4 @@
+export TARGET_HOST=https://replace_me
+export TARGET_PATH_REST=replace/me
+export TARGET_PATH_QUEUE=replace/me
+export ARTILLERY_API_KEY=replace_me
--- stress-test/test_fibonacci/processor.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  setPayload: function(context, events, done) {
-    context.vars.payload = {
-      n: parseInt(context.vars.n, 10),
-      times: parseInt(context.vars.times, 10)
-    };
-    return done();
-  }
-}
