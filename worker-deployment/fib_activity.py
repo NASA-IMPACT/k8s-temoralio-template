9bec3f6de1fda6308155044a7da09211ba9c98cd
amarouane-ABDELHAK <am0089@uah.edu>
2026-02-09 14:47:31 -0600

    Use httpx

--- worker-deployment/fib_activity.py
+++ worker-deployment/fib_activity.py
@@ -24,33 +24,30 @@ async def compute_fib(fib_input: FabInput) -> dict:
         raise ApplicationError("Invalid input, rolling back!", non_retryable=True)
 
     n = fib_input.n
-
+    url = "http://localhost:8000/api/predict/v1/fib/fibbonacci"
+    payload = {
+        "n": n,
+        "times": times
+    }
     results = []
-    for _ in range(times):
-        results.append(fib(n))
-    # url = "http://localhost:8000/api/predict/v1/fib/fibbonacci"
-    # payload = {
-    #     "n": n,
-    #     "times": times
-    # }
-    # async with httpx.AsyncClient(timeout=600.0) as client:
-    #     # Create the HTTP request task
-    #     request_task = asyncio.create_task(client.post(url, json=payload))
+    async with httpx.AsyncClient(timeout=600.0) as client:
+        # Create the HTTP request task
+        request_task = asyncio.create_task(client.post(url, json=payload))
         
-    #     # While the request is running, heartbeat every 15 seconds
-    #     while not request_task.done():
-    #         activity.heartbeat("Still waiting for API...")
-    #         # Wait for 15s or until the request finishes
-    #         done, _ = await asyncio.wait(
-    #             [request_task], 
-    #             timeout=15.0
-    #         )
-    #         if request_task in done:
-    #             break
+        # While the request is running, heartbeat every 15 seconds
+        while not request_task.done():
+            activity.heartbeat("Still waiting for API...")
+            # Wait for 15s or until the request finishes
+            done, _ = await asyncio.wait(
+                [request_task], 
+                timeout=15.0
+            )
+            if request_task in done:
+                break
 
-    #     resp = await request_task # Get the result (or raise exception)
-    #     resp.raise_for_status()
-    #     results = resp.json()
+        resp = await request_task # Get the result (or raise exception)
+        resp.raise_for_status()
+        results = resp.json()
     activity.logger.info(f"=====END ACTIVITY======")
     return {
         "result": f"Results of fib({n}) {times} times is {results}",
